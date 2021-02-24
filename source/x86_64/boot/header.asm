e90ce631d767bae4984d1e63083ba8a9917766cb
dh00mk3tu <cyberhybird@hotmail.com>
2021-02-25 02:24:59 +0530

    This commit is made by Auto-Git 3, Thursday 25 February 2021 02:22:52 AM IST

--- source/x86_64/boot/header.asm
+++ source/x86_64/boot/header.asm
@@ -3,4 +3,12 @@ header_start:
     dd 0xe85250d6 ; multiboot2
     ; arch
     dd 0 ; protected mode i386  
-
+    ; header len
+    dd header_end - header_start 
+    ; checksum
+    dd 0x10000000 - (0xe85250d6 + 0 + (header_end - header_start))
+    ; end tag
+    dw 0
+    dw 0
+    dd 8 
+header_end:
