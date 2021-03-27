973aaf578fbe8e86eee7f078ee92b67238045b87
dh00mk3tu <cyberhybird@hotmail.com>
2021-03-27 23:19:17 +0530

    This commit is made by Auto-Git 8, Sat 27 Mar 23:12:00 IST 2021

--- src/impl/x86_64/boot/main.asm
+++ src/impl/x86_64/boot/main.asm
@@ -3,9 +3,31 @@ global start
 section .text
 bits 32
 start:
-    mov esp. stack_top
+    mov esp. stack_top,
+    
+    call check_multiboot
+    call check_cpuid
+    call check_long_mode
+
     ; print 'AstraOS'
     mov dword [0xb8000], 0x2f4b2f4f
+    hlt
+
+check_multiboot:
+    cmp eax, 0x36d76289
+    jne .no_multiboot
+    ret
+
+.no_multiboot:
+    mov al, "M"
+    jmp error
+
+error:
+    ; print "ERR: X" where X is the error code
+    mov dword [0xb8000], 0x4f534f45
+    mov dword [0xb8004], 0x4f3a4f52
+    mov dword [0xb8008], 0x4f204f20
+    mov byte  [0xb800a], al
     hlt
 
 section .bss
