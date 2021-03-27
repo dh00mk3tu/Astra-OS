136604bbbc5a9624dadcd6b6ce6e0a9689e7f7d4
dh00mk3tu <cyberhybird@hotmail.com>
2021-03-27 23:44:16 +0530

    This commit is made by Auto-Git 32, Sat 27 Mar 23:12:00 IST 2021

--- src/impl/x86_64/boot/main.asm
+++ src/impl/x86_64/boot/main.asm
@@ -130,4 +130,8 @@ stack_top:
 section .rodata
 gdt64:
     dq 0 ; zero entry 
-    dq (1 << 43) | (1 << 44) | (1 << 47) | (1 << 53) ; code segment  
+.code_segment: equ $ - gdt64
+    dq (1 << 43) | (1 << 44) | (1 << 47) | (1 << 53) ; code segment
+.pointer: 
+    dw $ - gdt64 - 1
+    dq gdt64 
