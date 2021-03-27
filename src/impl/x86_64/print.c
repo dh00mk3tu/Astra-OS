4747b30ef903042c9dbfb87707e0e64f5c42b51c
dh00mk3tu <cyberhybird@hotmail.com>
2021-03-28 00:34:57 +0530

    This commit is made by Auto-Git 12, Sun 28 Mar 00:08:50 IST 2021

--- src/impl/x86_64/print.c
+++ src/impl/x86_64/print.c
@@ -1,5 +1,6 @@
 #include "print.h"
 
+const static size_t NUM_COLS = 80;
 struct Char {
     uint8_t character;
     uint8_t color; 
@@ -8,3 +9,4 @@ struct Char {
 struct Char* buffer = (struct Char*) 0xb8000;
 size_t col = 0;
 size_t row = 0;
+uint8_t color = PRINT_COLOR_WHITE | PRINT_COLOR_BLACK << 4;
