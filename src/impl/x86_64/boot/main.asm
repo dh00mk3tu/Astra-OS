a93570fd2b5b3cc4bb451899ba5295e74c4be942
dh00mk3tu <cyberhybird@hotmail.com>
2021-03-27 23:24:32 +0530

    This commit is made by Auto-Git 13, Sat 27 Mar 23:12:00 IST 2021

--- src/impl/x86_64/boot/main.asm
+++ src/impl/x86_64/boot/main.asm
@@ -44,6 +44,19 @@ check_cpuid:
 check_long_mode:
     mov eax, 0x80000000
     cpuid
+    cmp eax, 0x80000001
+    jb .no_long_mode
+    
+    mov eax, 0x80000001
+    cpuid 
+    test edx, 1 << 29
+    jz .no_long_mode
+    ret
+
+.no_long_mode:
+    mov al, "L"
+    jmp error
+
 error:
     ; print "ERR: X" where X is the error code
     mov dword [0xb8000], 0x4f534f45
