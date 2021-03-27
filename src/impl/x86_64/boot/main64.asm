274843ded5ae4348b8c60ef9dbf9ed9d51cf4f9b
dh00mk3tu <cyberhybird@hotmail.com>
2021-03-27 23:48:26 +0530

    This commit is made by Auto-Git 36, Sat 27 Mar 23:12:00 IST 2021

--- src/impl/x86_64/boot/main64.asm
+++ src/impl/x86_64/boot/main64.asm
@@ -3,3 +3,10 @@ global long_mode_start
 section .text 
 bits 64
 long_mode_start:
+    ; load null into all data segment registers
+    mov ax, 0
+    mov ss, ax
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
