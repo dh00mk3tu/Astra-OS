a4ecbb5e1fd03069b02abe178f6f641758db4a1d
dh00mk3tu <cyberhybird@hotmail.com>
2021-03-27 23:15:11 +0530

    This commit is made by Auto-Git 4, Sat 27 Mar 23:12:00 IST 2021

--- src/impl/x86_64/boot/main.asm
+++ src/impl/x86_64/boot/main.asm
@@ -3,7 +3,12 @@ global start
 section .text
 bits 32
 start:
+    mov esp. stack_top
     ; print 'AstraOS'
     mov dword [0xb8000], 0x2f4b2f4f
     hlt
 
+section .bss
+stack_bottom:
+    resb 4096 * 4
+stack_top:
