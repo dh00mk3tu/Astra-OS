80506d7067b2056a3d32627db7621b78516e5e10
dh00mk3tu <cyberhybird@hotmail.com>
2021-03-28 00:30:48 +0530

    This commit is made by Auto-Git 8, Sun 28 Mar 00:08:50 IST 2021

--- src/intf/print.h
+++ src/intf/print.h
@@ -1,5 +1,13 @@
 #pragma once 
 
+#include <stdint.h>
+#include <stddef.h>
+
+enum {
+    PRINT_COLOR_BLACK = 0,
+    PRINT_COLOR_YELLOW = 14,
+};
+
 void print_clear();
 void print_char(char character);
 void print_str(char* string);
