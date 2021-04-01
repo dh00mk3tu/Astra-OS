1bc0fc6781286f2bec60c55a1eac1ae30e38be9a
dh00mk3tu <cyberhybird@hotmail.com>
2021-04-01 17:35:04 +0530

    Using C to print text on the screen

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
-kernel_source_files := $(shell find src/impl/x86_64 -name *.asm)
-kernel_object_files := $(patsubst src/impl/x86_64/%.asm, build/x86_64/%.o, $(kernel_source_files))
+kernel_source_files := $(shell find src/impl/kernel -name *.c)
+kernel_object_files := $(patsubst src/impl/kernel/%.c, build/kernel/%.o, $(kernel_source_files))
 
 x86_64_c_source_files := $(shell find src/impl/x86_64 -name *.c)
 x86_64_c_object_files := $(patsubst src/impl/x86_64/%.c, build/x86_64/%.o, $(x86_64_c_source_files))
--- src/impl/x86_64/print.c
+++ src/impl/x86_64/print.c
@@ -5,7 +5,7 @@ const static size_t NUM_ROWS = 25;
 
 struct Char {
     uint8_t character;
-    uint8_t color; 
+    uint8_t color;
 };
 
 struct Char* buffer = (struct Char*) 0xb8000;
@@ -13,28 +13,27 @@ size_t col = 0;
 size_t row = 0;
 uint8_t color = PRINT_COLOR_WHITE | PRINT_COLOR_BLACK << 4;
 
-
-void clear_row(size_t row ){
+void clear_row(size_t row) {
     struct Char empty = (struct Char) {
-        Character: '',
+        character: ' ',
         color: color,
     };
 
-    for( size_t col = 0; col < NUM_COLS; col++ ){
+    for (size_t col = 0; col < NUM_COLS; col++) {
         buffer[col + NUM_COLS * row] = empty;
     }
 }
 
 void print_clear() {
-    for (size_t i = 0; i < NUM_COLS;i++) {
-        clear_row();
+    for (size_t i = 0; i < NUM_ROWS; i++) {
+        clear_row(i);
     }
 }
 
-void print_newLine() {
+void print_newline() {
     col = 0;
 
-    if(row < NUM_ROWS - 1) {
+    if (row < NUM_ROWS - 1) {
         row++;
         return;
     }
@@ -42,21 +41,21 @@ void print_newLine() {
     for (size_t row = 1; row < NUM_ROWS; row++) {
         for (size_t col = 0; col < NUM_COLS; col++) {
             struct Char character = buffer[col + NUM_COLS * row];
-            buffer[col + NUM_COLS * (row-1)] = character;
+            buffer[col + NUM_COLS * (row - 1)] = character;
         }
     }
 
     clear_row(NUM_COLS - 1);
 }
 
-void print_char() {
-    if(character == '\n'){
-        print_newLine();
+void print_char(char character) {
+    if (character == '\n') {
+        print_newline();
         return;
     }
 
-    if (col > NUM_COLS){
-        print_newLine();
+    if (col > NUM_COLS) {
+        print_newline();
     }
 
     buffer[col + NUM_COLS * row] = (struct Char) {
@@ -68,7 +67,7 @@ void print_char() {
 }
 
 void print_str(char* str) {
-    for(size_t i=0; 1; i++){
+    for (size_t i = 0; 1; i++) {
         char character = (uint8_t) str[i];
 
         if (character == '\0') {
@@ -79,6 +78,6 @@ void print_str(char* str) {
     }
 }
 
-void print_set_color (uint8_t foreground, uint8_t background) {
+void print_set_color(uint8_t foreground, uint8_t background) {
     color = foreground + (background << 4);
 }
